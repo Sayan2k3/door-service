f5ec20d9f32ef3228c4567c3b1c1f76465316341
Sayan Paul <121636215+Sayan2k3@users.noreply.github.com>
2025-03-20 00:24:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
-from flask import Flask , render_template
-
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 from applications import config
 from applications import models
-
 from applications import routes
-
-
-
 
 # @app.route('/')
 # def index():
 #     return render_template('prof.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
