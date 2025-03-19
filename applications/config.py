aeddff992e8a73cc620b5458ca535c585a8790bd
Sayan Paul <121636215+Sayan2k3@users.noreply.github.com>
2025-03-20 01:44:49 +0530

    Update config.py

--- applications/config.py
+++ applications/config.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-# from dotenv import dotenv_value
+from dotenv import dotenv_value
 import os
 from app import app
 
@@ -7,8 +7,8 @@ load_dotenv()
 
 curr_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(curr_dir)   
-# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 app.config['UPLOAD_EXTENSIONS']=['.pdf']
 app.config['UPLOAD_PATH']=os.path.join(curr_dir,'static',"pdfs")
